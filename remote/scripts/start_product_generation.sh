2d5998024aa040b832f04f47a87da3eee6bcf600
trerginl <lemiorhan.ergin@eu.sony.com>
2014-11-24 16:44:09 +0200

    added dynatrace parameters for TQA only

--- remote/scripts/start_product_generation.sh
+++ remote/scripts/start_product_generation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 WORKING_DIR="/opt/shared/scripts"
 
-sh ${WORKING_DIR}/start.sh -p 9095 -j octopus3-product-generation-service-app.jar -e $1 -l /opt/logs/product-generation -n octopus3-product-generation-service
+sh ${WORKING_DIR}/start.sh -p 9095 -j octopus3-product-generation-service-app.jar -e $1 -l /opt/logs/product-generation -n octopus3-product-generation-service -agentpath:/home/trbalika/dynatrace/agent/lib64/libdtagent.so=name=OCT_INT_TQA_APP3,server=192.168.52.105:9998
 
