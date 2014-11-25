24de4f2c5f0b3e861a6dac6e2a75238fdf22321e
trerginl <lemiorhan.ergin@eu.sony.com>
2014-11-25 13:29:16 +0200

    changed the path of start script. Devops folder will be used from now on.

--- remote/scripts/start_product_generation.sh
+++ remote/scripts/start_product_generation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-WORKING_DIR="/opt/shared/scripts"
+WORKING_DIR="/opt/shared/devops/scripts"
 
 sh ${WORKING_DIR}/start.sh -p 9095 -j octopus3-product-generation-service-app.jar -e $1 -l /opt/logs/product-generation -n octopus3-product-generation-service
 
