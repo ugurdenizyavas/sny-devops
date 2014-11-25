943dad8abcf45c9091fdf5c2efc5d65bac90e09b
trerginl <lemiorhan.ergin@eu.sony.com>
2014-11-25 13:52:17 +0200

    extra opts variable is defined

--- remote/scripts/start_product_generation.sh
+++ remote/scripts/start_product_generation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 WORKING_DIR="/opt/shared/devops/scripts"
+EXTRA_OPTS="-agentpath:/home/trbalika/dynatrace/agent/lib64/libdtagent.so=name=OCT_INT_TQA_APP3,server=192.168.52.105:9998"
 
-sh ${WORKING_DIR}/start.sh -p 9095 -j octopus3-product-generation-service-app.jar -e $1 -l /opt/logs/product-generation -n octopus3-product-generation-service -x "-agentpath:/home/trbalika/dynatrace/agent/lib64/libdtagent.so=name=OCT_INT_TQA_APP3,server=192.168.52.105:9998"
+sh ${WORKING_DIR}/start.sh -p 9095 -j octopus3-product-generation-service-app.jar -e $1 -l /opt/logs/product-generation -n octopus3-product-generation-service -x ${EXTRA_OPTS}
 
