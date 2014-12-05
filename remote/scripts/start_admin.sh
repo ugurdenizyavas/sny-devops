b45b732fb48155fc76d567555cd73ca4f487f220
trbasarb <baris.basaran@eu.sony.com>
2014-12-05 10:40:44 +0200

    added JVM_ARGS_EXTRA property

--- remote/scripts/start_admin.sh
+++ remote/scripts/start_admin.sh
@@ -2,5 +2,5 @@
 WORKING_DIR="/opt/shared/devops/scripts"
 
 
-sh ${WORKING_DIR}/start.sh -p 9096 -j octopus3-admin-app.jar -e $1 -l /opt/logs/admin -n octopus3-admin -wd ${WORKING_DIR}
+sh ${WORKING_DIR}/start.sh -p 9096 -j octopus3-admin-app.jar -l /opt/logs/admin -n octopus3-admin -wd ${WORKING_DIR}
 
